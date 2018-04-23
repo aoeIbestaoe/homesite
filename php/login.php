e8ea17512c6f0060650057cbf831dd3df1314376
Sander VP <sanderbogaert@hotmail.com>
2018-04-23 19:47:01 +0200

    revert + add test

--- php/login.php
+++ php/login.php
@@ -1,14 +1,18 @@
 <?php
-    $servername = "mysql:3306";
-    $username = "sander";
-    $password = "$@nd3r";
-    $dbname = "homepage";
+$servername = "mysql:3306";
+$username = "sander";
+$password = "$@nd3r";
+$dbname = "homepage";
 
-    try {
-        $mysqli = new mysqli($servername, $username, $password, $dbname);
-        echo "Connected successfully";
-    } catch(mysqli_sql_exception $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    $result = $conn->query("SHOW DATABASES");
+    echo $result;
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
 ?>
