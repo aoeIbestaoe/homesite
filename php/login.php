9fb87e741418292008744a066c7fd1b357e52af7
Sander VP <sanderbogaert@hotmail.com>
2018-04-24 20:39:57 +0200

    Fix another bug

--- php/login.php
+++ php/login.php
@@ -17,11 +17,11 @@ if ($mysqli->query("DROP TABLE login_credentials") === FALSE) {
 if ($mysqli->query("CREATE TABLE login_credentials ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30) NOT NULL, password VARCHAR(255) NOT NULL)") === FALSE) {
     echo "CREAT TABLE failed";
 }
-if ($mysqli->query("INSERT INTO login_credentials VALUES (0, 'Sander', '" . password_hash("Test123", PASSWORD_DEFAULT ) ."' )") === FALSE) {
+if ($mysqli->query("INSERT INTO login_credentials VALUES (0, 'sander', '" . password_hash("Test123", PASSWORD_DEFAULT ) ."' )") === FALSE) {
     echo "INSERT failed";
 }
 
-$pwd_to_check = $mysqli->query("SELECT password FROM login_credentials WHERE username = $login");
+$pwd_to_check = $mysqli->query("SELECT password FROM login_credentials WHERE username = '" . $login . "'");
 if ($pwd_to_check === FALSE) {
     echo "Unknown user";
 } else if ($pwd === $pwd_to_check) {
