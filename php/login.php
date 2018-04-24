8453c23afea659612821e8d5b9b6fd9522441112
Sander VP <sanderbogaert@hotmail.com>
2018-04-24 20:16:13 +0200

    Fix bug, enlarge pwd varchar to recommended

--- php/login.php
+++ php/login.php
@@ -10,10 +10,10 @@ if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 $login = filter_input(INPUT_POST, "username");
-$pwd = password_hash($_POST["password"]);
-
-$mysqli->query("CREATE TABLE login_credentials ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30) NOT NULL, password VARCHAR(30) NOT NULL)");
-$mysqli->query("INSERT INTO login_credentials (0, 'Sander', '" . password_hash("Test123") ."' )");
+$pwd = password_hash($_POST["password"], PASSWORD_DEFAULT );
+$mysqli->query("DROP TABLE login_credentials");
+$mysqli->query("CREATE TABLE login_credentials ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30) NOT NULL, password VARCHAR(255) NOT NULL)");
+$mysqli->query("INSERT INTO login_credentials (0, 'Sander', '" . password_hash("Test123", PASSWORD_DEFAULT ) ."' )");
 
 $pwd_to_check = $mysqli->query("SELECT password FROM login_credentials WHERE username = $login");
 if ($pwd_to_check === FALSE) {
