5b043793bef7dea7cdfd0980f7d22d1e2afb9562
Sander VP <sanderbogaert@hotmail.com>
2018-04-24 20:36:08 +0200

    more output

--- php/login.php
+++ php/login.php
@@ -11,9 +11,15 @@ if ($mysqli->connect_errno) {
 }
 $login = filter_input(INPUT_POST, "username");
 $pwd = password_hash($_POST["password"], PASSWORD_DEFAULT );
-$mysqli->query("DROP TABLE login_credentials");
-$mysqli->query("CREATE TABLE login_credentials ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30) NOT NULL, password VARCHAR(255) NOT NULL)");
-$mysqli->query("INSERT INTO login_credentials (0, 'Sander', '" . password_hash("Test123", PASSWORD_DEFAULT ) ."' )");
+if ($mysqli->query("DROP TABLE login_credentials") === FALSE) {
+    echo "DROP TABLE failed";
+}
+if ($mysqli->query("CREATE TABLE login_credentials ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30) NOT NULL, password VARCHAR(255) NOT NULL)") === FALSE) {
+    echo "CREAT TABLE failed";
+}
+if ($mysqli->query("INSERT INTO login_credentials (0, 'Sander', '" . password_hash("Test123", PASSWORD_DEFAULT ) ."' )") === FALSE) {
+    echo "INSERT failed";
+}
 
 $pwd_to_check = $mysqli->query("SELECT password FROM login_credentials WHERE username = $login");
 if ($pwd_to_check === FALSE) {
